b6f14fa1ea69a9ef9f9215a4a61a106c629bd61e
gsbka7 <95899871+gsbka7@users.noreply.github.com>
2022-04-27 00:20:13 +0800

    Update parallax.js

--- js/parallax.js
+++ js/parallax.js
@@ -12,20 +12,22 @@ const border = document.querySelector('.border')
 let header_height = header.offsetHeight;
 let section_height = section.offsetHeight;
 
-window.addEventListener('scroll', () => {
+window.addEventListener('scroll', function() {
     let scroll = window.pageYOffset;
     let sectionY = section.getBoundingClientRect();
-
-
+    console.log(sectionY)
+    
+    
     translate.forEach(element => {
         let speed = element.dataset.speed;
         element.style.transform = `translateY(${scroll * speed}px)`
+        
     });
-
+    
     opacity.forEach(element => {
         element.style.opacity = scroll / (sectionY.top + section_height);
     });
-
+    
     index_title.style.opacity = - scroll / (header_height / 2) + 1;
 
     shadow.style.height = `${scroll * 0.5 + 300}px`;
